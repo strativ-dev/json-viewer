3f6403836c7ac0afb6ee019a50e477f90b9d414f
masuk-al-hussain <masuk.hussain@strativ.se>
2025-12-24 17:08:11 +0600

    refactor table name column block

--- Block/Adminhtml/Form/Field/TableNameColumn.php
+++ Block/Adminhtml/Form/Field/TableNameColumn.php
@@ -1,33 +1,61 @@
 <?php
+
 namespace Strativ\JsonViewer\Block\Adminhtml\Form\Field;
 
+use Magento\Framework\View\Element\Context;
 use Magento\Framework\View\Element\Html\Select;
 use Strativ\JsonViewer\Model\Config\Source\DatabaseTables;
 
 class TableNameColumn extends Select
 {
-    protected $databaseTables;
+    /** @var DatabaseTables */
+    protected DatabaseTables $databaseTables;
 
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param DatabaseTables $databaseTables
+     * @param array $data
+     */
     public function __construct(
-        \Magento\Framework\View\Element\Context $context,
+        Context        $context,
         DatabaseTables $databaseTables,
-        array $data = []
-    ) {
+        array          $data = []
+    )
+    {
         parent::__construct($context, $data);
         $this->databaseTables = $databaseTables;
     }
 
-    public function setInputName($value)
+    /**
+     * Set the input name for the select
+     *
+     * @param string $value
+     * @return $this
+     */
+    public function setInputName($value): self
     {
         return $this->setName($value);
     }
 
-    public function setInputId($value)
+    /**
+     * Set the input id for the select
+     *
+     * @param string $value
+     * @return $this
+     */
+    public function setInputId(string $value): self
     {
         return $this->setId($value);
     }
 
-    protected function _toHtml()
+    /**
+     * Render the HTML for the select element
+     *
+     * @return string
+     */
+    protected function _toHtml(): string
     {
         $this->setOptions($this->databaseTables->toOptionArray());
         $this->setClass('admin__control-select');
