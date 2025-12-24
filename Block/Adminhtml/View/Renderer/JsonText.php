0d88550f2d34f236ba359d5219b976be92b71d8d
masuk-al-hussain <masuk.hussain@strativ.se>
2025-12-24 11:40:00 +0600

    Refactor JsonText file

--- Block/Adminhtml/View/Renderer/JsonText.php
+++ Block/Adminhtml/View/Renderer/JsonText.php
@@ -1,15 +1,43 @@
 <?php
+
 namespace Strativ\JsonViewer\Block\Adminhtml\View\Renderer;
 
+use Magento\Backend\Block\Context;
 use Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer;
 use Magento\Framework\DataObject;
+use Magento\Framework\Escaper;
 
 class JsonText extends AbstractRenderer
 {
-    public function render(DataObject $row)
+    /**
+     * @var Escaper
+     */
+    private Escaper $escaper;
+
+    /**
+     * @param Context $context
+     * @param Escaper $escaper
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Escaper $escaper,
+        array   $data = []
+    ) {
+        $this->escaper = $escaper;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Render the JSON text
+     *
+     * @param DataObject $row
+     * @return string
+     */
+    public function render(DataObject $row): string
     {
         $value = $row->getData($this->getColumn()->getIndex());
-        
+
         if (empty($value)) {
             return '';
         }
@@ -18,9 +46,9 @@ class JsonText extends AbstractRenderer
             $decoded = json_decode($value, true);
             if ($decoded !== null) {
                 $formattedJson = json_encode($decoded, JSON_PRETTY_PRINT);
-                $escapedJson = htmlspecialchars($formattedJson);
-                $uniqueId = 'json_' . md5($value . rand());
-                
+                $escapedJson = $this->escaper->escapeHtml($formattedJson);
+                $uniqueId = 'json_' . hash('sha256', $value . rand());
+
                 return '<button type="button" class="action-default scalable" '
                     . 'onclick="showJsonModal(\'' . $uniqueId . '\')" '
                     . 'style="padding: 5px 10px;">'
@@ -31,16 +59,22 @@ class JsonText extends AbstractRenderer
         }
 
         $displayText = strlen($value) > 25 ? substr($value, 0, 25) . '...' : $value;
-        return '<span>' . htmlspecialchars($displayText) . '</span>';
+        return '<span>' . $this->escaper->escapeHtml($displayText) . '</span>';
     }
 
-    protected function isJson($string)
+    /**
+     * Check if a string is valid JSON
+     *
+     * @param mixed $string
+     * @return bool
+     */
+    protected function isJson(mixed $string): bool
     {
         if (!is_string($string)) {
             return false;
         }
-        
+
         json_decode($string);
-        return (json_last_error() == JSON_ERROR_NONE);
+        return json_last_error() === JSON_ERROR_NONE;
     }
 }
