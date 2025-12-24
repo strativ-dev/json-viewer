5acb317cfadcac0b30d88b27eda7da82af3d2c04
masuk-al-hussain <masuk.hussain@strativ.se>
2025-12-24 18:51:00 +0600

    refactor populatebutton

--- Block/Adminhtml/System/Config/PopulateButton.php
+++ Block/Adminhtml/System/Config/PopulateButton.php
@@ -1,32 +1,61 @@
 <?php
+
 namespace Strativ\JsonViewer\Block\Adminhtml\System\Config;
 
+use Magento\Backend\Block\Template\Context;
+use Magento\Backend\Block\Widget\Button;
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\LocalizedException;
 
 class PopulateButton extends Field
 {
+    /**
+     * @var string
+     */
     protected $_template = 'Strativ_JsonViewer::system/config/populate_button.phtml';
-    protected $scopeConfig;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected ScopeConfigInterface $scopeConfig;
 
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param array $data
+     */
     public function __construct(
-        \Magento\Backend\Block\Template\Context $context,
-        array $data = []
-    ) {
+        Context $context,
+        array   $data = []
+    )
+    {
         parent::__construct($context, $data);
         $this->scopeConfig = $context->getScopeConfig();
     }
 
-    protected function _getElementHtml(AbstractElement $element)
+    /**
+     * Render the button HTML
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element): string
     {
         return $this->_toHtml();
     }
 
-    public function getButtonHtml()
+    /**
+     * Generate the button HTML
+     *
+     * @return string
+     * @throws LocalizedException
+     */
+    public function getButtonHtml(): string
     {
         $button = $this->getLayout()->createBlock(
-            \Magento\Backend\Block\Widget\Button::class
+            Button::class
         )->setData([
             'id' => 'populate_tables_button',
             'label' => __('Populate Table Columns'),
@@ -36,20 +65,25 @@ class PopulateButton extends Field
         return $button->toHtml();
     }
 
-    public function getSavedTableColumns()
+    /**
+     * Retrieve saved table columns configuration
+     *
+     * @return string
+     */
+    public function getSavedTableColumns(): string
     {
         $savedData = $this->scopeConfig->getValue(
             'strativ_jsonviewer/general/table_columns',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
-        
+
         if ($savedData) {
             $data = json_decode($savedData, true);
             if (is_array($data)) {
                 return json_encode($data);
             }
         }
-        
+
         return '{}';
     }
 }
